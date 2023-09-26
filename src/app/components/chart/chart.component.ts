e517fc5d2d99ad0cd23713a5cd77edac3b0ea803
itsmeneartou <kartikwatwani3@gmail.com>
2023-09-26 16:09:54 +0530

    Improve the Todo.

--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -580,7 +580,7 @@ export class ChartComponent {
 }
 
 
-// TODO: In "ByPresence", For the By Day & Hour table, there are three select options. Increase the width of the table to match the width of the total width of 3 select options.
+// TODO: In "ByPresence", For the "Top Requesters By Day & Hour" table, there are three select options. Increase the width of the table to match the width of the total width of 3 select options. Also, experiment If other tables need to be increased in width to improve the UI or only increasing the width of "Top Requesters By Day & Hour" table keeps the aesthetics of the whole base component good.
 
 // TODO: Move reactionList to constants.ts.
 
